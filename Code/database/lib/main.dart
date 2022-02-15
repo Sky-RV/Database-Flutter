f1529f6c62ed5c40e7f64d8693cfab7bc388a9ab
Sky <90393918+Sky-RV@users.noreply.github.com>
2022-02-15 19:28:45 +0330

    Buttons

--- Code/database/lib/main.dart
+++ Code/database/lib/main.dart
@@ -22,6 +22,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  TextEditingController IDtxt = TextEditingController();
+  TextEditingController NAMEtxt = TextEditingController();
+  TextEditingController PHONEtxt = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,8 +43,173 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
 
-      ////////////////////////////// BODy //////////////////////////////
+      ////////////////////////////// BODY //////////////////////////////
       body: Container(
+        child: SingleChildScrollView(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.height,
+                width: double.infinity,
+
+                child: Column(
+                  children: [
+
+                    ////////////////////////////// INPUTS //////////////////////////////
+
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 5),
+                      child: Column(
+                        children: [
+
+                          SizedBox(height: 30,),
+
+                          MYinput(label: 'ID', cnt: IDtxt, avatar: Icon(Icons.assignment_ind_outlined)),
+                          SizedBox(height: 15,),
+
+                          MYinput(label: 'Name', cnt: NAMEtxt, avatar: Icon(Icons.account_circle)),
+                          SizedBox(height: 15,),
+
+                          MYinput(label: 'Phone', cnt: PHONEtxt, avatar: Icon(Icons.phone)),
+                          SizedBox(height: 15,)
+
+                        ],
+                      ),
+                    ),
+
+                    ////////////////////////////// BUTTONS //////////////////////////////
+
+                    SizedBox(height: 50,),
+
+                    // Save
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: ElevatedButton(
+                        onPressed: (){},
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.zero,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                        ),
+                        child: Ink(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Color(0xFF6F2C7B),
+                                Color(0xFF1720C2)
+                              ],
+                            ),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Container(
+                            width: 360,
+                            height: 40,
+                            alignment: Alignment.center,
+                            child: Text('Save', style: TextStyle(fontSize: 13, color: Colors.white),),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 25,),
+
+                    // Update
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: ElevatedButton(
+                        onPressed: (){},
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.zero,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                        ),
+                        child: Ink(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Color(0xFF6F2C7B),
+                                Color(0xFF1720C2)
+                              ],
+                            ),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Container(
+                            width: 360,
+                            height: 40,
+                            alignment: Alignment.center,
+                            child: Text('Update', style: TextStyle(fontSize: 13, color: Colors.white),),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 25,),
+
+                    // Delete
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: ElevatedButton(
+                        onPressed: (){},
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.zero,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                        ),
+                        child: Ink(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Color(0xFF7B2C42),
+                                Color(0xFFC21717)
+                              ],
+                            ),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Container(
+                            width: 360,
+                            height: 40,
+                            alignment: Alignment.center,
+                            child: Text('Delete', style: TextStyle(fontSize: 13, color: Colors.white),),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 25,),
+
+                    // List
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: ElevatedButton(
+                        onPressed: (){},
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.zero,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                        ),
+                        child: Ink(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Color(0xFF6F2C7B),
+                                Color(0xFF1720C2)
+                              ],
+                            ),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Container(
+                            width: 360,
+                            height: 40,
+                            alignment: Alignment.center,
+                            child: Text('List', style: TextStyle(fontSize: 13, color: Colors.white),),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 30,),
+
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+
+        ////////////////////////////// BACKGROUND //////////////////////////////
+
         decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topRight,
@@ -57,3 +226,17 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+Widget MYinput({label, cnt, avatar}){
+  return TextField(
+    controller: cnt,
+    style: TextStyle(color: Colors.black),
+    decoration: InputDecoration(
+        focusColor: Color(0xFF050052),
+        labelText: label,
+        prefixIcon: avatar,
+        labelStyle: TextStyle(color: Color(0xFF051242)),
+    ),
+  );
+}
+
